074d5268e10a68e98aefc4f4120a95723edf0551
Edward Shih <ultralabed@gmail.com>
2016-11-15 14:09:56 +0800

    Add login form and action/reducer flow.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import firebase from 'firebase';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
   componentWillMount() {
@@ -19,11 +19,7 @@ class App extends Component {
   render() {
     return (
       <Provider store={createStore(reducers)}>
-        <View>
-          <Text>
-            hi
-          </Text>
-        </View>
+        <LoginForm />
       </Provider>
     );
   }
